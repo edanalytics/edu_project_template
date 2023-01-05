6d55a4108b015a867c84cf31c840b7dc8db333da
jkaiser <jayckaiser@gmail.com>
2023-01-05 08:55:33 -0600

    Remove superfluous pathing punctuation from local repo installation commands for clarity.

--- init/venv-airflow-init.sh
+++ init/venv-airflow-init.sh
@@ -23,13 +23,13 @@ cd ~/code
 git clone https://github.com/edanalytics/ea_airflow_util.git
 git clone https://github.com/edanalytics/edu_edfi_airflow.git
 
-pip install -e ./ea_airflow_util
-pip install -e ./edu_edfi_airflow
+pip install -e ea_airflow_util
+pip install -e edu_edfi_airflow
 
 # Checkout the most recent tagged releases.
 # UPDATE THESE LINES AS VERSIONS INCREMENT.
-git -C ./ea_airflow_util  checkout tags/v0.1.0
-git -C ./edu_edfi_airflow checkout tags/v0.1.0
+git -C ea_airflow_util  checkout tags/v0.1.0
+git -C edu_edfi_airflow checkout tags/v0.1.0
 
 # Return to the original path.
 cd -
