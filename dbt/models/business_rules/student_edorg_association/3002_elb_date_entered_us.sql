6b482e4db73ffd2dda0fae7d5477f63bd4abfc90
Steven McKee <steven.mckee@tnedu.gov>
2025-01-10 10:58:01 -0600

    TEDS-972 - Fixed a bug with how rule 3002 works. It was using the wrong field.

--- dbt/models/business_rules/student_edorg_association/3002_elb_date_entered_us.sql
+++ dbt/models/business_rules/student_edorg_association/3002_elb_date_entered_us.sql
@@ -7,25 +7,19 @@
 
 {% set error_code = 3002 %}
 
-/* Students with below characteristics must have Date Entered US populated. */
+/* Students with below LEP Codes must have Date Entered US populated. */
 with stg_student_edorgs as (
     select *
     from {{ ref('stg_ef3__student_education_organization_associations_orig') }} seoa
     where k_lea is not null
+        and lep_code in ('L','W','1','2','3','4','F','N')
         {{ school_year_exists(error_code, 'seoa') }}
 )
 select se.k_student, se.k_lea, se.k_school, se.school_year, se.ed_org_id, se.student_unique_id,
     {{ error_code }} as error_code,
-    concat('ELB Students with codes [L, W, 1, 2, 3, 4, F, N] require Date Entered US on District level Student/EdOrg Associations.') as error,
+    concat('ELB Students with LEP codes [L, W, 1, 2, 3, 4, F, N] require Date Entered US on District level Student/EdOrg Associations.') as error,
     {{ error_severity_column(error_code, 'se') }}
 from stg_student_edorgs se
 join {{ ref('edu_edfi_source', 'stg_ef3__students') }} s
     on se.k_student = s.k_student
 where s.date_entered_us is null
-    and exists (
-            select 1
-            from {{ ref('stg_ef3__stu_ed_org__characteristics') }} sc
-            where sc.k_lea = se.k_lea
-                and sc.k_student = se.k_student
-                and sc.student_characteristic in ('L','W','1','2','3','4','F','N')
-        )
