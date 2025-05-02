c461e1d0fde18378e58f57c9046250237c48a105
rlittle08 <rlittle@edanalytics.org>
2025-05-02 14:48:20 -0500

    feature/python_312_airflow_293 (#45)
    * Update venv-airflow-init.sh
    
    * Update venv-dbt-init.sh

--- init/venv-dbt-init.sh
+++ init/venv-dbt-init.sh
@@ -1,6 +1,6 @@
 echo Beginning venv-dbt-init.sh
 
 ### set up dbt environment ###
-python3.8 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install wheel setuptools  --quiet
 pip install dbt-core dbt-postgres dbt-snowflake  --quiet
