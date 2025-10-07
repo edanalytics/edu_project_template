522c77bfb18648d6b1e12ecb7c7750c78618e13d
thanuja bachala <thanuja.bachala@tnedu.gov>
2025-10-07 18:26:33 -0500

    TEDS-1186 - Implement 3200_Discipline action dates only for I, S and not for R, E Discipline actions_10_7_2025

--- dbt/models/business_rules/discipline_actions/3200_discipline_date.sql
+++ dbt/models/business_rules/discipline_actions/3200_discipline_date.sql
@@ -16,7 +16,10 @@ with brule as (
     where br.tdoe_error_code = {{ error_code }}
 ),
 stg_discipline_actions as (
-    select * from {{ ref('stg_ef3__discipline_actions_orig') }} da
+    select *
+           ,{{ edu_edfi_source.extract_descriptor('value:disciplineDescriptor::string') }} as discipline_action
+    from {{ ref('stg_ef3__discipline_actions_orig') }} da
+         , lateral variant_explode(v_disciplines)
     where exists (
         select 1
         from brule
@@ -34,10 +37,13 @@ select da.k_student, da.k_school__responsibility, da.school_year,
         'does not fall within the school year. Value Received: ', da.discipline_date, '. The state school year starts ',
       concat(cast((da.school_year-1) as int), '-07-01'), ' and ends ', concat(cast(da.school_year as int), '-06-30'), '.') as error,
     brule.tdoe_severity as severity
+    ,discipline_action
 from stg_discipline_actions da
 join {{ ref('stg_ef3__students') }} s
     on s.k_student = da.k_student
 join brule
     on da.school_year between brule.error_school_year_start and brule.error_school_year_end
 where not(da.discipline_date between to_date(concat(cast((da.school_year-1) as int), '-07-01'), 'yyyy-MM-dd') 
-    and to_date(concat(cast(da.school_year as int), '-06-30'), 'yyyy-MM-dd'))
+          and to_date(concat(cast(da.school_year as int), '-06-30'), 'yyyy-MM-dd'))
+      and discipline_action IN ('I','S')
+      and discipline_action NOT IN ('R','E')
