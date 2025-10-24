623d7549ae4cedb918bd512d16b3971ec3154a1c
Steven McKee <steven.mckee@tnedu.gov>
2025-10-24 10:51:32 -0500

    TEDS-1193 - Fix bugs with date_entered_us due to EA's CDS impl.

--- dbt/models/custom_data_sources/cds_student_additional_columns.sql
+++ dbt/models/custom_data_sources/cds_student_additional_columns.sql
@@ -7,7 +7,7 @@
 
 select s.k_student, 
     s.state_student_id as stateStudentId,
-    s.date_entered_us as date_entered_us
+    s.date_entered_us as dateenteredus
 from {{ ref('stg_ef3__students') }} s
 where s.state_student_id is not null
     or s.date_entered_us is not null
