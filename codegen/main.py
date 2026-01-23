12e9c94f91f3d39d97c9e9701de66244705e0338
Jacob Bortell <jbortell@edanalytics.org>
2026-01-23 14:07:47 -0600

    Generate templates from local swagger

--- codegen/main.py
+++ codegen/main.py
@@ -6,17 +6,38 @@
 # ]
 # ///
 
+import json
 import os
 import pathlib
-import yaml
 
+import yaml
 from edfi_api_client import EdFiClient
 from edfi_api_client import camel_to_snake
+from edfi_api_client.edfi_swagger import EdFiSwagger
 
-from template_util import load_template, write_template
+from .template_util import load_template
+from .template_util import write_template
 
 
-def generate_templates(base_url, api_version=3, database=None, schema=None):
+def get_swagger_from_path(swagger_path: str) -> dict[str, EdFiSwagger]:
+    with open(swagger_path, "r") as fp:
+        swagger = json.load(fp)
+
+    return {
+        "resources": EdFiSwagger("resources", swagger),
+        "descriptors": EdFiSwagger("descriptors", swagger),
+    }
+
+
+def generate_templates_from_swagger_path(swagger_path: str, database=None, schema=None):
+    generate_templates_from_swagger_data(
+        get_swagger_from_path(swagger_path), database=database, schema=schema
+    )
+
+
+def generate_templates_from_swagger_data(
+    swagger_data: dict[str, EdFiSwagger], database=None, schema=None
+):
     """
     Populate blank templates with information found in the Swagger doc.
         Note: Each template-generation uses local variables as kwargs to minimize formatting-boilerplate.
@@ -27,20 +48,18 @@ def generate_templates(base_url, api_version=3, database=None, schema=None):
     """
     ### Build directories to save filled templates
     base_dir = pathlib.Path(__file__).resolve().parents[1]
-    TEMPLATES_DIR = os.path.join(base_dir, 'codegen', 'blank')
-    GENERATED_DIR = os.path.join(base_dir, 'codegen', 'generated')
-    CONFIGS_DIR   = os.path.join(base_dir, 'airflow', 'configs')
+    TEMPLATES_DIR = os.path.join(base_dir, "codegen", "blank")
+    GENERATED_DIR = os.path.join(base_dir, "codegen", "generated")
+    CONFIGS_DIR = os.path.join(base_dir, "airflow", "configs")
 
-
-    ### Build the resources and descriptors swaggers, and extract the necessary info from each
-    api = EdFiClient(base_url, api_version=api_version)
-    resources_swagger   = api.get_swagger('resources')
-    descriptors_swagger = api.get_swagger('descriptors')
+    resources_swagger = swagger_data["resources"]
+    descriptors_swagger = swagger_data["descriptors"]
 
     RESOURCES = [
         (namespace, resource)
         for namespace, resource in resources_swagger.endpoints
-        if 'Descriptor' not in resource  # Be extra careful to separate resources from descriptors
+        if "Descriptor"
+        not in resource  # Be extra careful to separate resources from descriptors
     ]
     RESOURCE_DELETES = resources_swagger.deletes
     RESOURCE_DESCRIPTIONS = resources_swagger.descriptions
@@ -48,7 +67,8 @@ def generate_templates(base_url, api_version=3, database=None, schema=None):
     DESCRIPTORS = [
         (namespace, descriptor)
         for namespace, descriptor in descriptors_swagger.endpoints
-        if 'Descriptor' in descriptor  # Be extra careful to separate resources from descriptors
+        if "Descriptor"
+        in descriptor  # Be extra careful to separate resources from descriptors
     ]
 
     REFERENCE_SKEYS = {  # Make references snake-cased to match their formatting in the warehouse
@@ -58,12 +78,11 @@ def generate_templates(base_url, api_version=3, database=None, schema=None):
 
     print("Successfully gathered all namespaces and definitions from Swagger.")
 
-
     ### Populate each of the templates.
     # EDFI DESCRIPTOR CONFIG BLOCK
-    file = 'edfi_descriptors.yml'
+    file = "edfi_descriptors.yml"
     template_path = os.path.join(TEMPLATES_DIR, file)
-    output_path   = os.path.join(CONFIGS_DIR, file)
+    output_path = os.path.join(CONFIGS_DIR, file)
 
     template = load_template(template_path)
     formatted = []
@@ -72,36 +91,36 @@ def generate_templates(base_url, api_version=3, database=None, schema=None):
 
     write_template(output_path, formatted)
 
-
     # EDFI RESOURCE CONFIG BLOCK
-    file = 'edfi_resources.yml'
+    file = "edfi_resources.yml"
     template_path = os.path.join(TEMPLATES_DIR, file)
-    output_path   = os.path.join(CONFIGS_DIR, file)
+    output_path = os.path.join(CONFIGS_DIR, file)
 
     template = load_template(template_path)
     formatted = []
     for namespace, name in RESOURCES:
-        has_deletes = ((namespace, name) in RESOURCE_DELETES)
+        has_deletes = (namespace, name) in RESOURCE_DELETES
         formatted.append(template.format(**locals()))
 
     write_template(output_path, formatted)
 
-
     ### Ensure resources that are required by edu_edfi_source are added to 'src_edfi_3.yml' and 'sql_source_create_table.sql'
-    with open('./required_resources.yml') as fp:
+    required_resources_path = os.path.join(
+        base_dir, "codegen", "required_resources.yml"
+    )
+    with open(required_resources_path) as fp:
         required_resources = yaml.safe_load(fp)
 
     for name in required_resources:
-        resource = ('ed-fi', name)
+        resource = ("ed-fi", name)
         if resource not in RESOURCES:
             RESOURCES.append(resource)
             RESOURCE_DESCRIPTIONS[name] = required_resources[name]
 
-
     # DBT SOURCE PROPERTY BLOCK
-    file = 'src_edfi_3.yml'
+    file = "src_edfi_3.yml"
     template_path = os.path.join(TEMPLATES_DIR, file)
-    output_path   = os.path.join(GENERATED_DIR, file)
+    output_path = os.path.join(GENERATED_DIR, file)
 
     template = load_template(template_path, indent=2)
     formatted = []
@@ -112,27 +131,25 @@ def generate_templates(base_url, api_version=3, database=None, schema=None):
 
     write_template(output_path, formatted)
 
-
     # SQL SOURCE CREATE TABLE BLOCK
-    file = 'sql_source_create_table.sql'
+    file = "sql_source_create_table.sql"
     template_path = os.path.join(TEMPLATES_DIR, file)
-    output_path   = os.path.join(GENERATED_DIR, file)
+    output_path = os.path.join(GENERATED_DIR, file)
 
     template = load_template(template_path)
     formatted = []
     for namespace, name in RESOURCES:
         snake = camel_to_snake(name)
-        database = database or 'raw'
-        schema = schema or 'edfi3'
+        database = database or "raw"
+        schema = schema or "edfi3"
         formatted.append(template.format(**locals()))
 
     write_template(output_path, formatted)
 
-
     # EDU_EDFI_SOURCE GEN_SKEY REFERENCE COLUMNS
-    file = 'gen_skey__reference_columns.yml'
+    file = "gen_skey__reference_columns.yml"
     template_path = os.path.join(TEMPLATES_DIR, file)
-    output_path   = os.path.join(GENERATED_DIR, file)
+    output_path = os.path.join(GENERATED_DIR, file)
 
     template = load_template(template_path)
     formatted = []
@@ -142,14 +159,35 @@ def generate_templates(base_url, api_version=3, database=None, schema=None):
 
     write_template(output_path, formatted)
 
-
     ### All done.
     print("All templates written!")
 
 
+def generate_templates(base_url, api_version=3, database=None, schema=None):
+    """
+    Populate blank templates with information found in the Swagger doc.
+        Note: Each template-generation uses local variables as kwargs to minimize formatting-boilerplate.
 
-if __name__ == '__main__':
+    :param base_url:
+    :param api_version:
+    :param database:
+    :param schema:
+    :return:
+    """
+    ### Build the resources and descriptors swaggers, and extract the necessary info from each
+    api = EdFiClient(base_url, api_version=api_version)
+    resources_swagger = api.get_swagger("resources")
+    descriptors_swagger = api.get_swagger("descriptors")
 
+    swagger_data = {
+        "resources": resources_swagger,
+        "descriptors": descriptors_swagger,
+    }
+
+    generate_templates_from_swagger_data(swagger_data, database=database, schema=schema)
+
+
+if __name__ == "__main__":
     import sys
     base_url = sys.argv[1]
     database = sys.argv[2]
